ee3e8d47d5a4590e78a85e240d88f48045bf62c6
Camille MANEL <camille.manel@gmail.com>
2025-05-14 10:32:26 +0000

    function use

--- macros/function.sql
+++ macros/function.sql
@@ -1,3 +1,3 @@
 {% macro margin_percent(revenue, purchase_cost, decimals=2) %}
-    return(ROUND(SAFE_DIVIDE(({{revenue}} - {{purchase_cost}}), {{revenue}}), {{decimals}}))
+    ROUND(SAFE_DIVIDE(({{revenue}} - {{purchase_cost}}), {{revenue}}), {{decimals}}) * 100
  {% endmacro %}
